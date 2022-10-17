393c06f4955a67e8ce374cefccb98177c22db11c
Nicolas Batista Piñeyro <nicolas26386@gmail.com>
2022-10-17 02:23:35 -0300

    v1.02

--- src/app/modules/home-page/components/projects/projects.component.ts
+++ src/app/modules/home-page/components/projects/projects.component.ts
@@ -17,11 +17,17 @@ export class ProjectsComponent implements OnInit {
   public vue2: string = `Vue 2`;
   public vue3: string = `Vue 3`;
 
-  public pageName1: string = `Estadios del Uruguay`;
-  public linkPage1: string = `https://estadiosdeluruguay.netlify.app/`;
+  public pageName1: string = `Simpsons Quotes API`;
+  public linkPage1: string = `https://simpsonsquotesapi.netlify.app/`;
+  public githubRepo1: string = `https://github.com/nicolas5746/simpsonsquotesapi/`;
 
-  public pageName2: string = `Automotora Uruguay`;
-  public linkPage2: string = `https://automotora-uruguay.netlify.app/`;
+  public pageName2: string = `Estadios del Uruguay`;
+  public linkPage2: string = `https://estadiosdeluruguay.netlify.app/`;
+  public githubRepo2: string = `https://github.com/nicolas5746/estadios/`;
+
+  public pageName3: string = `Automotora Uruguay`;
+  public linkPage3: string = `https://automotora-uruguay.netlify.app/`;
+  public githubRepo3: string = `https://github.com/nicolas5746/automotora-uruguay/`;
 
   public projectsPicture = {
     imageAlt: `My Projects`,
