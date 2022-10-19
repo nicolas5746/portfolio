1410013d1fa19870c9c9e2ccd3725dd8123ab793
Nicolas Batista Piñeyro <nicolas26386@gmail.com>
2022-10-19 01:23:25 -0300

    v1.09

--- src/app/shared/components/scroll-down/scroll-down.component.ts
+++ src/app/shared/components/scroll-down/scroll-down.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-down',
@@ -7,13 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./scroll-down.component.sass'],
 })
 export class ScrollDownComponent implements OnInit {
-  @Input() route: string = '';
-
-  constructor(private router: Router) {}
-
-  goTo(): void {
-    this.router.navigate([this.route]);
-  }
-
   ngOnInit(): void {}
 }
